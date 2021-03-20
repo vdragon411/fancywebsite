1ee0d95f49e16b7b6751bc186a4defe329d7ce0c
Vinay Patil <vinaypatil41198@gmail.com>
2021-03-20 18:55:44 +0530

    getting somewhere

--- js/index.js
+++ js/index.js
@@ -1,144 +1,125 @@
 "use strict";
 
-var signal = (function () {
-    var signals = {};
-    return {
-        subscribe: function (key, func) {
-            if (!signals[key]) {
-                signals[key] = {};
-            }
+import signal from "./signal.js";
 
-            var index = 0;
-            var len = Object.keys(signals[key]).length;
-            if (len > 0) {
-                index = parseInt(Object.keys(signals[key])[len - 1]) + 1;
-            }
-            signals[key][index] = func;
-            this.debugPrint();
 
-            // var unsubscribed = false;
-            return {
-                unsubscribe: function () {
-                    // if (!unsubscribed) {
-                        delete signals[key][index];
-                        console.log("unsubcribing", index);
-                        // unsubscribed = true;
-                    // }
-                }
-            }
-        },
-        emit: function (signal, data) {
-            for (var key in signals[signal]) {
-                signals[signal][key](data);
-            }
-            this.debugPrint();
-        },
-        signals: signals,
-        debugPrint: function () {
-            document.getElementById("debug").innerHTML = "";
-            for (var signal in signals) {
-                document.getElementById("debug").innerHTML += "<ul>Signal: "+signal;
-                for (var idx in signals[signal]) {
-                    document.getElementById("debug").innerHTML += "<li>"+idx + "</li>";
-                }
-                document.getElementById("debug").innerHTML += "</ul>";
-            }
+class Widget { 
+    constructor(tag, classes=[]) {
+        this.tag = tag = tag || "div";
+	classes = classes || [];
+        this.elem = document.createElement(tag);
+        classes.forEach(c => this.elem.classList.add(c));
+        [
+            "onclick",
+            "onmouseover",
+            "onmousedown",
+	    ].forEach(f => {
+            if (this[f]) this.elem[f] = this[f].bind(this);
+        });
+    }
+    addClass(...classes) {
+        this.elem.classList.add(...classes);
+    }
+    addChild(child) {
+        this.elem.appendChild(child.elem);
+    }
+    setParent(parent) {
+        parent.elem.appendChild(this.elem);
+    }
+}
+
+class Panel extends Widget {
+    constructor() {
+        super('div', ["panel", "horizontal", "noselect"]);
+        this.setParent({elem:document.body});
+
+        let btn = new PanelButton();
+        this.addChild(btn);
+
+        let btn2 = new PanelButton();
+        this.addChild(btn2);
+    }
+}
+
+class PanelButton extends Widget {
+    constructor() {
+        super('div', ["hoverable"]);
+        let btn = new Widget("img" ,["relative-center"]);
+        btn.elem.src = "assets/start-win.svg";
+        this.addChild(btn);
+        this.btn = btn;
+
+        let menu = new PanelMenu();
+        this.addChild(menu);
+        this.menu = menu;
+    }
+    onclick(e) {
+        e.stopPropagation();
+        if (!this.clicked) {
+            this.activate();
+        } else {
+            this.deactivate();
         }
     }
-})();
+    activate() {
+        this.clicked = true;
+        this.elem.classList.add("hover");
 
+        this.menu.open();
 
-function inherit (derived, base) {
-    derived.prototype = Object.create(base.prototype);
-    derived.prototype.constructor = derived;
-}
-
-function Widget (tag = "div") {
-    this.elem = document.createElement(tag);
-}
-Widget.prototype.addChild = function (child) {
-    this.elem.appendChild(child.elem);
-}
-Widget.prototype.addClass = function (...classes) {
-    this.elem.classList.add(...classes);
-}
-Widget.prototype.setParent = function (parent) {
-    parent.appendChild(this.elem);
-}
-
-
-
-
-function Panel () {
-    Widget.call(this);
-    this.addClass("panel", "horizontal", "noselect");
-    this.setParent(document.body);
-
-    var btn = new PanelButton();
-    this.addChild(btn);
-    var btn2 = new PanelButton();
-    this.addChild(btn2);
-    var btn3 = new PanelButton();
-    this.addChild(btn3);
-
-    btn2.elem.style.display = "none";
-    btn.actionObj = btn2.elem;
-
-    btn3.elem.style.display = "none";
-    btn2.actionObj = btn3.elem;
-
-    document.onclick = function (e) {
-        signal.emit("unfocus", e.target);
+        let widget = this;
+        signal.subscribe("unfocus", () => {
+            widget.deactivate();
+        });
     }
+    deactivate() {
+        this.clicked = false;
 
-}; inherit(Panel, Widget);
+        let elem = this.elem;
+        this.elem.classList.remove("hoverable");
+        this.menu.close().then( _ => {
+            this.elem.classList.add("hoverable");   
+        });
+        this.elem.classList.remove("hover");
 
-function PanelButton () {
-    var elem, icon;
-
-    Widget.call(this);
-    this.addClass("hoverable");
-
-    icon = new Widget("img");
-    icon.elem.src = "assets/start-win.svg";
-    icon.addClass("relative-center");
-    this.addChild(icon);
-
-    this.elem.onclick = this.click.bind(this);
-    this.clicked = false;
-
-}; inherit(PanelButton, Widget);
-PanelButton.prototype.click = function (e) {
-    if (this.clicked) {
-        this.close();
-    } else {
-        e.stopPropagation();
-        this.open();
     }
 }
-PanelButton.prototype.open = function () {
-    this.sig = signal.subscribe("unfocus", (function (d) {
-        this.close();
-    }).bind(this));
-    this.clicked = true;
-    this.highlightOn();
-    if (this.actionObj) {
-        this.actionObj.style.display = "block";
+
+class PanelMenu extends Widget {
+    constructor () {
+        super('div', ["start-menu"]);
+        this.displayType = this.elem.style.display;
+        this.elem.style.display = "none";
+
+        this.elem.onclick = e => e.stopPropagation();
     }
-}
-PanelButton.prototype.close = function () {
-    this.clicked = false;
-    this.highlightOff();
-    if (this.sig) this.sig.unsubscribe();
-    if (this.actionObj) {
-        this.actionObj.style.display = "none";
+    open() {
+        return new Promise( (resolve, reject) => {
+            this.elem.style.display = this.displayType;
+            this.elem.style.animationDuration = "0.5s";
+            this.elem.style.animationFillMode = "forwards";
+            this.elem.style.animationName = "visible";
+            this.elem.addEventListener("animationend", () => {
+                resolve();
+            }, {once: true});
+        });
     }
-}
-PanelButton.prototype.highlightOn = function () {
-    this.elem.classList.add("hover");
-}
-PanelButton.prototype.highlightOff = function () {
-    this.elem.classList.remove("hover");
+    close() {
+        return new Promise( (resolve, reject) => {
+            this.elem.offsetHeight;
+            this.elem.style.animationDuration = "0.5s";
+            this.elem.style.animationFillMode = "forwards";
+            this.elem.style.animationName = "visible2";
+            this.elem.addEventListener("animationend", () => {
+                this.elem.style.display = "none";
+                resolve();
+            }, {once: true});
+        });
+    }
 }
 
-var panel = new Panel();
+
+document.onclick = () => {
+    signal.emit('unfocus');
+}
+let panel = new Panel();
