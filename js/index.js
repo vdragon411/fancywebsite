364ee0884d7b9edd36569f1839cd20fad63413bc
vdragon411 <vinaypatil41198@gmail.com>
2021-05-13 19:49:00 +0530

    simpler css

--- js/index.js
+++ js/index.js
@@ -1,225 +1,95 @@
-"use strict";
+class Element {
+    constructor(tag, ...classes) {
+        this.elem = document.createElement(tag);
+        this.elem.classList.add(...classes);
+    }
 
-import signal from "./signal.js";
+    addChildElem(child) {
+        this.elem.appendChild(child);
+    }
+    addChild(child) {
+        this.addChildElem(child.elem);
+    }
+    setParentElem(parent) {
+        parent.appendChild(this.elem);
+    }
+    setParent(parent) {
+        this.setParentElem(parent.elem);
+    }
 
-import {Widget, animate} from "./widget.js";
 
+}
 
-class Panel extends Widget {
-    constructor(classes=[]) {
-        super('div', ["panel", "horizontal", "noselect", ...classes]);
-        // this.setParent({elem:document.body});
-
-        let startBtn = new PanelIconToggle("assets/start-win.svg");
-        let menu = new StartMenu();
-        menu.setToggle(startBtn);
-
-        let btn = new PanelIconButton("assets/apps/chrome.svg");
-        btn.whenOn = () => {
-            console.log("clicky");
-            startBtn.activate();
-        }
-
-        this.addChild(
-            startBtn,
-            btn
-        );
+class Div extends Element {
+    constructor(...classes) {
+        super('div', ...classes);
     }
 }
 
-class PanelIcon extends Widget {
-    constructor(path, classes=[], shouldHover=true) {
-        super("div", [...classes]);
-        if (shouldHover) this.addClass("hoverable");
-        let icn = new Widget("img" ,["relative-center"]);
-        icn.elem.src = path;
-        this.addChild(icn);
-    }
-}
-
-class PanelIconToggle extends PanelIcon {
-    constructor(path, classes=[]) {
-        super(path, classes);
-        this.whenOn = null;
-        this.whenOff = null;
-    }
-    onclick(e) {
-        e.stopPropagation();
-        if (!this.clicked) {
-            this.activate();
-        } else {
-            this.deactivate();
-        }
-    }
-    activate() {
-        this.clicked = true;
-        this.elem.classList.add("hover");
-
-        if (this.whenOn) {
-            this.whenOn();
-        }
-        let widget = this;
-        signal.subscribe("unfocus", () => {
-            widget.deactivate();
-        });
-    }
-    deactivate() {
-        this.clicked = false;
-
-        this.elem.classList.remove("hoverable");
-        if (this.whenOff) {
-            this.whenOff();
-        }
-        this.elem.classList.remove("hover");
-    }
-}
-
-class PanelIconButton extends PanelIcon {
-    constructor(path, classes=[]) {
-        super(path, classes);
-        this.whenOn = null;
-    }
-    onclick(e) {
-        e.stopPropagation();
-        if (this.whenOn) this.whenOn();
+class Img extends Element {
+    constructor(url, ...classes) {
+        super('img', ...classes);
+        this.elem.src = url;
     }
 }
 
 
+// let panel = new Div('panel');
+// let panel_icon = new Div('panel-item', 'panel-icon');
+// let panel_icon_img = new Img('assets/start-win.svg', 'relative-center');
 
+class PanelIcon extends Div {
+    constructor(url, ...classes) {
+        super('panel-icon', ...classes);
 
-class PanelMenu extends Widget {
-    constructor (classes = []) {
-        super('div', [...classes]);
-        this.elem.style.display = "none";
-        this.elem.onclick = e => e.stopPropagation();
-    }
-    setToggle(btn) {
-        btn.whenOn = () => {
-            this.open().then(()=>{
-            });
-        }
-        btn.whenOff = () => {
-            this.close().then(_=>{
-                btn.elem.classList.add("hoverable");
-            })
-        }
-        btn.addChild(this);
-    }
-    open() {
-        return new Promise((resolve, reject) => {
-            if (this.elem.style.display != "none")
-                this.elem.style.display = "none";
-
-            this.elem.style.display = "";
-            animate(this.elem, "fadeInUp", "0.3s").start()
-            .then(_=>{
-                resolve();
-            });
-        });
-    }
-    close() {
-        return new Promise((resolve, reject) => {
-            animate(this.elem, "fadeOutDown", "0.3s").start()
-            .then((() => {
-                resolve();
-                if (this.elem.style.display != "")
-                    this.elem.style.display = "";
-                this.elem.style.display = "none";
-            }).bind(this));
-        });
+        let img = new Img(url, 'relative-center');
+        this.addChild(img);
     }
 }
 
-
-class StartMenuLeft extends Widget {
+class Panel extends Div {
     constructor() {
-        super("div", ["start-menu--left"]);
+        super('panel', "bottom");
 
-        let main = new Widget("div", ["start-menu--left__main"]);
-        let icons = new Widget("div", ["start-menu--left__icons"]);
-        let expand = new Widget("div", ["start-menu--left__expand"]);
-        main.addChild(icons, expand);
+        let addIcon = (func) => {
+            this.test = new PanelIcon("assets/start-win.svg", "hoverable");
+            this.addChild(this.test);
 
-        let icon = new PanelIcon("assets/start-win.svg", ["icon50x50"]);
-        icons.addChild(icon);
-
-        this.addChild(main);
+            this.test.elem.onclick = func;
+        }
+        let resetAlignment = _ => {
+            this.elem.classList.remove("bottom");
+            this.elem.classList.remove("left");
+            this.elem.classList.remove("top");
+            this.elem.classList.remove("right");
+        }
+        addIcon( () => {
+            resetAlignment();
+            this.elem.classList.add("left");
+        });
+        addIcon( () => {
+            resetAlignment();
+            this.elem.classList.add("bottom");
+        });
+        addIcon( () => {
+            resetAlignment();
+            this.elem.classList.add("top");
+        });
+        addIcon( () => {
+            resetAlignment();
+            this.elem.classList.add("right");
+        });
     }
 }
 
-class StartMenuMiddle extends Widget {
-    constructor()  {
-        super("div",["start-menu--middle"]);
-        // this.elem.style.backgroundColor = "red";
-
-        let addItem = (iconPath, itemText) => {
-            let item = new Widget("div", ["hoverable"]);
-            let icon = new PanelIcon(iconPath, ["start-menu--middle__icon"], false);
-            let text = new Widget("div", ["start-menu--middle__iconText"]);
-            text.elem.innerText = itemText;
-            item.addChild(icon);
-            item.addChild(text);
-            this.addChild(item);
-        }
-        let addText = (itemText) => {
-            let item = new Widget("div", ["hoverable"]);
-            let text = new Widget("div", ["start-menu--middle__text"]);
-            text.elem.innerText = itemText;
-            item.addChild(text);
-            this.addChild(item);
-        }
-        addText("Recently Used");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addText("test");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-        addItem("assets/apps/chrome.svg", "Google Chrome");
-    }
-}
-
-class StartMenu extends PanelMenu {
+class Desktop extends Div {
     constructor() {
-        super(["start-menu"]);
-        let leftDiv = new StartMenuLeft();
-        let MidDiv = new StartMenuMiddle();
+        super('desktop');
+        this.setParentElem(document.body);
 
-        this.addChild(leftDiv,MidDiv);
+        let panel = new Panel();
+        this.addChild(panel);
     }
 }
 
-
-document.onclick = () => {
-    signal.emit('unfocus');
-}
-let panel = new Panel();
-let desktop = document.createElement("div");
-let wallpaper = new Widget('div', ["wallpaper"]);
-wallpaper.elem.style.backgroundImage = "url(assets/wallpaper-win.jpg)";
-desktop.appendChild(wallpaper.elem);
-desktop.appendChild(panel.elem);
-document.body.appendChild(desktop);
+let desktop = new Desktop();
