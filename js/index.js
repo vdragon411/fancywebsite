0b7136d6ce6e9b93bd8996fffbd85f844595ef9c
vdragon411 <vinaypatil41198@gmail.com>
2021-05-27 14:43:55 +0530

    compact design

--- js/index.js
+++ js/index.js
@@ -1,95 +1,115 @@
-class Element {
-    constructor(tag, ...classes) {
+
+class Elem {
+    constructor(tag, classes=[]) {
         this.elem = document.createElement(tag);
-        this.elem.classList.add(...classes);
+        this.addClass(...classes);
     }
-
-    addChildElem(child) {
-        this.elem.appendChild(child);
+    addClass(...classes) {
+        classes.forEach(c => this.elem.classList.add(c));
+        return this;
     }
-    addChild(child) {
-        this.addChildElem(child.elem);
+    add(...children) {
+        children.forEach(c => this.elem.appendChild(c.elem));
+        return this;
     }
-    setParentElem(parent) {
-        parent.appendChild(this.elem);
-    }
-    setParent(parent) {
-        this.setParentElem(parent.elem);
-    }
-
-
-}
-
-class Div extends Element {
-    constructor(...classes) {
-        super('div', ...classes);
+    setProperty(prop, value) {
+        this.elem.style.setProperty(prop, value);
+        return this;
     }
 }
 
-class Img extends Element {
-    constructor(url, ...classes) {
-        super('img', ...classes);
-        this.elem.src = url;
+class Flex extends Elem {
+    constructor(tag, classes=[]) {
+        super(tag, classes);
+    }
+    add(...children) {
+        this.addClass('flex-main');
+        return super.add(...children.map(c => c.addClass('flex-item')));
     }
 }
 
 
-// let panel = new Div('panel');
-// let panel_icon = new Div('panel-item', 'panel-icon');
-// let panel_icon_img = new Img('assets/start-win.svg', 'relative-center');
 
-class PanelIcon extends Div {
-    constructor(url, ...classes) {
-        super('panel-icon', ...classes);
-
-        let img = new Img(url, 'relative-center');
-        this.addChild(img);
+class Div extends Elem {
+    constructor(classes=[]) {
+        super('div', classes);
     }
 }
 
-class Panel extends Div {
-    constructor() {
-        super('panel', "bottom");
-
-        let addIcon = (func) => {
-            this.test = new PanelIcon("assets/start-win.svg", "hoverable");
-            this.addChild(this.test);
-
-            this.test.elem.onclick = func;
-        }
-        let resetAlignment = _ => {
-            this.elem.classList.remove("bottom");
-            this.elem.classList.remove("left");
-            this.elem.classList.remove("top");
-            this.elem.classList.remove("right");
-        }
-        addIcon( () => {
-            resetAlignment();
-            this.elem.classList.add("left");
-        });
-        addIcon( () => {
-            resetAlignment();
-            this.elem.classList.add("bottom");
-        });
-        addIcon( () => {
-            resetAlignment();
-            this.elem.classList.add("top");
-        });
-        addIcon( () => {
-            resetAlignment();
-            this.elem.classList.add("right");
-        });
+class FlexDiv extends Flex {
+    constructor(classes=[]) {
+        super('div', classes);
     }
 }
 
-class Desktop extends Div {
-    constructor() {
-        super('desktop');
-        this.setParentElem(document.body);
 
-        let panel = new Panel();
-        this.addChild(panel);
+class Icon extends Flex {
+    constructor(iconType, classes=[], imgClasses=[]) {
+        super('div', classes.concat(['iconP', 'flex-center'])).add(
+            this.sub = new Elem(iconType, imgClasses.concat(['flex-noitem','icon']))
+        )
+    }
+    path(iconPath) {
+        return this.setProperty('--path', "url("+iconPath+")");
     }
 }
 
-let desktop = new Desktop();
+let panel = new FlexDiv(['panel']).add(
+    new Icon('div', ['hoverable']).path("/assets/start-win.svg"),
+    new FlexDiv(['cortana', 'flex-noitem']).add(
+        new FlexDiv(['flex-center']).add(
+            new Div(['circle']),
+        ),
+        this.text = new Elem('textarea', ['flex-noitem']),
+        new Icon('div', ['flex-noitem', 'mic']).path("/assets/mic.png")
+    ),
+    new Icon('div', ['hoverable'], ['invert']).path("/assets/task-view.png"),
+    new FlexDiv().add(
+        new Icon('div', ['hoverable']).path("/assets/apps/chrome.svg"),
+        new Icon('div', ['hoverable']).path("/assets/apps/chrome.svg"),
+        new Icon('div', ['hoverable']).path("/assets/apps/chrome.svg"),
+        new Icon('div', ['hoverable']).path("/assets/apps/chrome.svg"),
+    ),
+    this.mid = new Div(['panel-mid']),
+    this.date = new Elem('center', ['datetime','hoverable', 'flex-noitem']),
+    this.showdesktop = new Div(['flex-noitem', 'showdesktop', 'hoverable']),
+);
+
+this.showdesktop.elem.onclick = e => {
+    panel.elem.classList.toggle('vertical');
+}
+
+
+
+setInterval( _ => {
+    let dateObj = new Date();
+    let currentTime = dateObj.toLocaleTimeString([],
+        {
+            hour12: true,
+            hour: '2-digit',
+            minute: '2-digit'
+        }).toUpperCase();
+    if (currentTime[0] === "0") {
+        currentTime = currentTime.slice(1);
+    }
+    let currentDate = dateObj.toLocaleDateString([], {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit'
+    });
+    date.elem.innerHTML = currentTime + "<br>" + currentDate;
+}, (60 - new Date().getSeconds()) * 1000);
+
+date.elem.innerHTML = `
+3:12 PM<br>
+17/05/21
+`;
+
+
+text.elem.placeholder = 'Type to search here';
+text.elem.rows = "1";
+
+panel.elem.style.setProperty('--path', "url(/assets/wallpaper-apple.jpg)");
+
+
+document.body.appendChild(panel.elem);
